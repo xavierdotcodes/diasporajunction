d61ac295dc9dc15948c02e0e03336c40a8dd8c28
Xavier Cooper <xcc2b@virginia.edu>
2025-09-26 12:31:57 +0000

    troubleshooting redis

--- src/lib/server/redis.js
+++ src/lib/server/redis.js
@@ -1,3 +1,9 @@
 import Redis from 'ioredis';
 import { env } from '$env/dynamic/private';
-export const redis = new Redis(env.REDIS_URL);
+
+const redisUrl = env.REDIS_URL;
+if (!redisUrl) {
+	throw new Error('Missing REDIS_URL in environment');
+}
+
+export const redis = new Redis(redisUrl);
