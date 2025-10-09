968d2605a6bebf7ecabadc7901f9ec37d47aad4a
Xavier Cooper <xcc2b@virginia.edu>
2025-10-09 04:38:51 +0000

    adding favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,6 +30,13 @@
 		content="Discover opportunities, events, and networks tailored for the African diaspora. Build connections, grow, and thrive globally."
 	/>
 	<meta name="twitter:image" content="https://diasporajunxion.com/logo2.png" />
+
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="DiasporaJunxion" />
+	<link rel="manifest" href="/site.webmanifest" />
 </svelte:head>
 
 <!-- The important part -->
