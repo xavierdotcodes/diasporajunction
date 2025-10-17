db9b2a98780add9e6a184286d51f6dfbabda5f65
Xavier Cooper <xcc2b@virginia.edu>
2025-10-17 20:03:25 +0000

    hiding blog and diasporaUnited

--- src/lib/layout/Navbar.svelte
+++ src/lib/layout/Navbar.svelte
@@ -7,10 +7,10 @@
 	const links = [
 		{ href: '/space', label: 'SP△CE' },
 		{ href: '/ndgo', label: 'NDGO' },
-		{ href: '/diasporaunited', label: 'Diaspora United' },
+		//{ href: '/diasporaunited', label: 'Diaspora United' },
 		{ href: '/tours', label: 'Tours' },
 		{ href: '/team', label: 'Team' },
-		{ href: '/blog', label: 'Blog' },
+		//{ href: '/blog', label: 'Blog' },
 
 		{ href: '/sekondi', label: 'We ❤️ Sekondi' },
 		{ href: '/support', label: 'How to Support' },
